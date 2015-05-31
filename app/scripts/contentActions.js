65d2ca88520eeabfdbf4f23a392bf5d24378ea1a
Nir Natan <nirn@wix.com>
2015-05-31 13:13:58 +0300

    add debug=react to the url if it's missing

--- app/scripts/contentActions.js
+++ app/scripts/contentActions.js
@@ -1,6 +1,84 @@
 'use strict';
+/*!
+ * querystring - Simple querystring lib with no dependencies
+ * v0.1.0
+ * https://github.com/jgallen23/querystring
+ * copyright Greg Allen 2013
+ * MIT License
+ */
+var querystring = {
+    parse: function (string) {
+        var parsed = {};
+        string = (string !== undefined) ? string : window.location.search;
+
+        if (typeof string === "string" && string.length > 0) {
+            if (string[0] === '?') {
+                string = string.substring(1);
+            }
+
+            string = string.split('&');
+
+            for (var i = 0, length = string.length; i < length; i++) {
+                var element = string[i],
+                    eqPos = element.indexOf('='),
+                    keyValue, elValue;
+
+                if (eqPos >= 0) {
+                    keyValue = element.substr(0, eqPos);
+                    elValue = element.substr(eqPos + 1);
+                }
+                else {
+                    keyValue = element;
+                    elValue = '';
+                }
+
+                elValue = decodeURIComponent(elValue);
+
+                if (parsed[keyValue] === undefined) {
+                    parsed[keyValue] = elValue;
+                }
+                else if (parsed[keyValue] instanceof Array) {
+                    parsed[keyValue].push(elValue);
+                }
+                else {
+                    parsed[keyValue] = [parsed[keyValue], elValue];
+                }
+            }
+        }
+
+        return parsed;
+    },
+    stringify: function (obj) {
+        var string = [];
+
+        if (!!obj && obj.constructor === Object) {
+            for (var prop in obj) {
+                if (obj[prop] instanceof Array) {
+                    for (var i = 0, length = obj[prop].length; i < length; i++) {
+                        string.push([encodeURIComponent(prop), encodeURIComponent(obj[prop][i])].join('='));
+                    }
+                }
+                else {
+                    string.push([encodeURIComponent(prop), encodeURIComponent(obj[prop])].join('='));
+                }
+            }
+        }
+
+        return string.join('&');
+    }
+};
 
 require(['react', 'lodash'], function (React, _) {
+    if (!_.has(React.addons, 'TestUtils')) {
+        var params = querystring.parse(window.location.search);
+        if (params.debug) {
+            params.debug = _.isArray(params.debug) ? params.debug.push('react') : [params.debug, 'react'];
+        } else {
+            params.debug = 'react';
+        }
+
+        window.location.search = querystring.stringify(params);
+    }
     function compactObject(obj) {
         return _.transform(obj, function (acc, value, key) {
             if (value) {
@@ -82,7 +160,6 @@ require(['react', 'lodash'], function (React, _) {
                 contextPath: comp.contextPath
             };
             return JSON.parse(JSON.prune(compactObject(result)));
-            //return _.omit(window.selectedComponent.props, ['siteAPI', 'siteData', 'children', 'pageData', 'loadedStyles']);
         }
     };
 
