868add8f182c97f563d465ef483536d271621741
Nir Natan <nirn@wix.com>
2015-07-15 04:55:44 +0300

    require minified files

--- app/scripts/require-config.js
+++ app/scripts/require-config.js
@@ -1,9 +1,9 @@
 requirejs.config({
     'baseUrl': '/',
     'paths': {
-        jquery: '/bower_components/jquery/jquery',
-        react: '/bower_components/react/react-with-addons',
-        lodash: '/bower_components/lodash/lodash',
+        jquery: '/bower_components/jquery/dist/jquery.min',
+        react: '/bower_components/react/react-with-addons.min',
+        lodash: '/bower_components/lodash/lodash.min',
         jsoneditor: '/bower_components/jsoneditor/dist/jsoneditor',
         text: '/bower_components/requirejs-plugins/lib/text',
         json: '/bower_components/requirejs-plugins/src/json',
