d5493df30039ea61a5fb94b52390fa0d8bfd2951
Nir Natan <nirn@wix.com>
2015-08-22 19:31:04 +0300

    moved options to scripts folder

--- app/scripts/require-config.js
+++ app/scripts/require-config.js
@@ -7,9 +7,9 @@ requirejs.config({
         jsoneditor: '/bower_components/jsoneditor/dist/jsoneditor',
         text: '/bower_components/requirejs-plugins/lib/text',
         json: '/bower_components/requirejs-plugins/src/json',
-        dataHandler: '/packages/options/dataHandler',
+        dataHandler: '/scripts/options/dataHandler',
         generated: '/scripts/generated',
-        options: '/packages/options',
+        options: '/scripts/options',
         popup: 'scripts/popup',
         utils: 'scripts/utils'
     }
