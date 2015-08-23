8632ed35d32a72549e0e9103312c74839f3c8dc8
Nir Natan <nirn@wix.com>
2015-08-23 11:18:51 +0300

    refactor options page to use bootstrap components

--- app/scripts/require-config.js
+++ app/scripts/require-config.js
@@ -2,7 +2,7 @@ requirejs.config({
     'baseUrl': '/',
     'paths': {
         jquery: '/bower_components/jquery/dist/jquery.min',
-        react: '/bower_components/react/react-with-addons.min',
+        react: '/bower_components/react/react-with-addons',
         lodash: '/bower_components/lodash/lodash.min',
         jsoneditor: '/bower_components/jsoneditor/dist/jsoneditor',
         text: '/bower_components/requirejs-plugins/lib/text',
@@ -11,6 +11,7 @@ requirejs.config({
         generated: '/scripts/generated',
         options: '/scripts/options',
         popup: 'scripts/popup',
-        utils: 'scripts/utils'
+        utils: 'scripts/utils',
+        'react-bootstrap': '/bower_components/react-bootstrap/react-bootstrap'
     }
 });
