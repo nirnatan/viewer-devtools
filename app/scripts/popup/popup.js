32ed8f46cbe70ea1e871e97b78dea47931417327
Nir Natan <nirn@wix.com>
2015-11-13 17:52:26 +0200

    replace React.render with ReactDOM.render (react 0.14 update)

--- app/scripts/popup/popup.js
+++ app/scripts/popup/popup.js
@@ -1,7 +1,7 @@
-requirejs(['react', 'lodash', 'popup/app'], function (React, _, app) {
+requirejs(['react', 'react-dom', 'lodash', 'popup/app'], function (React, ReactDOM, _, app) {
     'use strict';
 
     var appElement = React.createElement(app, {debuggable: true});
-    window.rendered = React.render(appElement, document.getElementById('root'));
+    window.rendered = ReactDOM.render(appElement, document.getElementById('root'));
 });
 
