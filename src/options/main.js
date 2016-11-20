2557266afb8b790c295e60f3317177c1604785a1
Nir Natan <nirn@wix.com>
2016-11-20 18:56:55 +0200

    Add GoogleAnalytics support with pageview for popup and options pages

--- src/options/main.js
+++ src/options/main.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectGoogleAnalytics from '../scripts/ga';
 import store from '../store/store';
 import Options from './Options';
 
 injectTapEventPlugin();
+injectGoogleAnalytics('/options.html');
 
 const render = () => {
   ReactDOM.render(
