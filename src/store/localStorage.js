91a1dece150e417020dcbf9b49711fa0a114ed0d
Nir Natan <nirn@wix.com>
2016-11-21 13:59:30 +0200

    Sync localStorage with features and versions on init

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -1,6 +1,7 @@
 import Immutable from 'immutable';
-import { mapValues } from 'lodash';
+import { get, mapValues } from 'lodash';
 import { initFromStorage } from './actions/index';
+import { requestFeatures, requestVersions } from './utils';
 
 export const updateStorage = store => next => action => {
   const result = next(action);
@@ -8,7 +9,28 @@ export const updateStorage = store => next => action => {
   return result;
 };
 
+const updateFeatures = (store) => {
+  requestFeatures().then(features => {
+    const currentState = mapValues(store.getState(), v => v.toJS());
+    const newFeatures = mapValues(features, (value, key) => Object.assign(value, { active: get(currentState.features, [key, 'active'], false) }));
+    const devtools = Object.assign(currentState, { features: newFeatures });
+    chrome.storage.local.set({ devtools });
+  });
+};
+
+const updateVersions = (store) => {
+  requestVersions().then(({ editor, viewer }) => {
+    const devtools = mapValues(store.getState(), v => v.toJS());
+    devtools.versions.editor.versions = editor.length > 0 ? editor.reverse().slice(0, 100) : devtools.versions.editor.versions;
+    devtools.versions.viewer.versions = viewer.length > 0 ? viewer.reverse().slice(0, 100) : devtools.versions.viewer.versions;
+    chrome.storage.local.set({ devtools });
+  });
+};
+
 export const init = store => {
+  updateVersions(store);
+  updateFeatures(store);
+
   chrome.storage.local.get('devtools', ({ devtools }) => {
     if (devtools) {
       store.dispatch(initFromStorage(mapValues(devtools, v => Immutable.fromJS(v))));
