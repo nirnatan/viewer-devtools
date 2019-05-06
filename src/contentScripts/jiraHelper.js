fffe56f74d996fe0bb82d43aa3e50ed2e2c48fb1
Nir Natan <nirn@wix.com>
2019-05-06 12:03:12 +0300

    auto login to Jira

--- src/contentScripts/jiraHelper.js
+++ src/contentScripts/jiraHelper.js
@@ -1,5 +1,11 @@
 (() => {
-  Array.from(document.querySelectorAll('a'))
+  const anchors = Array.from(document.getElementsByTagName('a'));
+  const login = anchors.find(x => x.innerText.startsWith('Sign in'));
+  if (login) {
+    login.click();
+  }
+
+  anchors
     .filter(a => a.href.indexOf('jira.wixpress.com') === -1)
     .forEach(anchor => {
       anchor.target = '_blank';
