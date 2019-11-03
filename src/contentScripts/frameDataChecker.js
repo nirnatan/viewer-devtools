d1319b4c82ab22e018ecc4e92d549ab0eb047e64
Nir Natan <nirn@wix.com>
2019-11-03 17:46:25 +0200

    check if frame is bolt or santa by querying the main-r script element

--- src/contentScripts/frameDataChecker.js
+++ src/contentScripts/frameDataChecker.js
@@ -1,15 +1,47 @@
-window.onload = () => {
-  let counter = 0;
-  const int = setInterval(() => {
-    counter++;
-    if (window.boltInstance) {
-      clearInterval(int);
-      window.parent.postMessage('bolt', '*');
+/* global _ */
+(() => {
+  const createIdicator = event => {
+    if (event.data === 'bolt' || event.data === 'santa') {
+      const isBolt = event.data === 'bolt';
+      const boltItitle = `Click will switch experiment to ${isBolt ? 'Santa' : 'Bolt'}`;
+      let boltI = document.querySelector('div.boltIndicator');
+      if (boltI) {
+        document.body.removeChild(boltI);
+      }
+
+      boltI = document.createElement('div');
+      boltI.classList.add('boltIndicator');
+      boltI.setAttribute('alt', boltItitle);
+      boltI.setAttribute('title', boltItitle);
+      boltI.textContent = event.data;
+      boltI.addEventListener('click', () => {
+        let url = location.href;
+        if (!url.toLowerCase().includes('petri_ovr')) {
+          url = `${url}&petri_ovr=specs.UseBoltInPreview:${isBolt ? 'false' : 'true'}`;
+        } else {
+          url = url.replace(/(petri_ovr=).*?(&|$)/, `$1specs.UseBoltInPreview:${isBolt ? 'false' : 'true'}$2`);
+        }
+
+        location.assign(url);
+      });
+
+      document.body.appendChild(boltI);
+      window.removeEventListener('message', createIdicator);
     }
-    // assuming we are on santa
-    if (counter > 10) {
-      clearInterval(int);
-      window.parent.postMessage('santa', '*');
+  };
+
+  function inIframe() {
+    try {
+      return window !== window.top;
+    } catch (e) {
+      return true;
     }
-  }, 300);
-};
+  }
+
+  if (inIframe()) {
+    const boltScript = document.querySelector('script[src*="bolt-main/app/main-r.min.js"]');
+    window.parent.postMessage(boltScript ? 'bolt' : 'santa', '*');
+  } else {
+    window.addEventListener('message', createIdicator);
+  }
+})();
