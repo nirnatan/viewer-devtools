f86f34bf180652fdfa0566df7e1084a05e8509aa
Nir Natan <nirn@wix.com>
2019-12-26 10:10:37 +0200

    add boltIndicator to standalone preview frame and to app builder

--- src/contentScripts/frameDataChecker.js
+++ src/contentScripts/frameDataChecker.js
@@ -1,32 +1,36 @@
 /* global _ */
 (() => {
-  const createIdicator = event => {
-    if (event.data === 'bolt' || event.data === 'santa') {
-      const isBolt = event.data === 'bolt';
-      const boltItitle = `Click will switch experiment to ${isBolt ? 'Santa' : 'Bolt'}`;
-      let boltI = document.querySelector('div.boltIndicator');
+  const createIndicator = event => {
+    if (event.data === "bolt" || event.data === "santa") {
+      const isBolt = event.data === "bolt";
+      const boltItitle = `Click will switch experiment to ${
+        isBolt ? "Santa" : "Bolt"
+      }`;
+      let boltI = document.querySelector("div.boltIndicator");
       if (boltI) {
         document.body.removeChild(boltI);
       }
 
-      boltI = document.createElement('div');
-      boltI.classList.add('boltIndicator');
-      boltI.setAttribute('alt', boltItitle);
-      boltI.setAttribute('title', boltItitle);
+      boltI = document.createElement("div");
+      boltI.classList.add("boltIndicator");
+      boltI.setAttribute("alt", boltItitle);
+      boltI.setAttribute("title", boltItitle);
       boltI.textContent = event.data;
-      boltI.addEventListener('click', () => {
+      boltI.addEventListener("click", () => {
         let url = location.href;
-        if (!url.toLowerCase().includes('petri_ovr')) {
-          url = `${url}&petri_ovr=specs.UseBoltInPreview:${isBolt ? 'false' : 'true'}`;
+        const isBoltString = isBolt ? "false" : "true";
+        const petriOverrides = `specs.UseBoltInPreview:${isBoltString};specs.useBoltInAppBuilderPreview:${isBoltString}`;
+        if (!url.toLowerCase().includes("petri_ovr")) {
+          url = `${url}&petri_ovr=${petriOverrides}`;
         } else {
-          url = url.replace(/(petri_ovr=).*?(&|$)/, `$1specs.UseBoltInPreview:${isBolt ? 'false' : 'true'}$2`);
+          url = url.replace(/(petri_ovr=).*?(&|$)/, `$1${petriOverrides}$2`);
         }
 
         location.assign(url);
       });
 
       document.body.appendChild(boltI);
-      window.removeEventListener('message', createIdicator);
+      window.removeEventListener("message", createIndicator);
     }
   };
 
@@ -39,9 +43,15 @@
   }
 
   if (inIframe()) {
-    const boltScript = document.querySelector('script[src*="bolt-main/app/main-r.min.js"]');
-    window.parent.postMessage(boltScript ? 'bolt' : 'santa', '*');
+    const boltScript = document.querySelector(
+      'script[src*="bolt-main/app/main-r.min.js"]'
+    );
+    window.parent.postMessage(boltScript ? "bolt" : "santa", "*");
   } else {
-    window.addEventListener('message', createIdicator);
+    const boltScript = document.querySelector(
+      'script[src*="bolt-main/app/main-r.min.js"]'
+    );
+    createIndicator({ data: boltScript ? "bolt" : "santa" });
+    window.addEventListener("message", createIndicator);
   }
 })();
