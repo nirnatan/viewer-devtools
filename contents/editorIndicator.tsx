150ba1eab21a8c09048a3ee65108b0107074f01b
Nir <nirn@wix.com>
2022-08-01 11:50:26 +0300

    Fix editor indicator css

--- contents/editorIndicator.tsx
+++ contents/editorIndicator.tsx
@@ -1,5 +1,4 @@
 import { Box } from '@mui/material'
-import cssText from 'data-text:~/contents/indicator.css'
 import type { PlasmoContentScript } from 'plasmo'
 import { Storage, useStorage } from '@plasmohq/storage'
 import { createSetPetriOvr } from '~utils/urlManager'
@@ -18,40 +17,63 @@ const createElement = (type: string, props: Record<string, any>) => {
   return element
 }
 
+const addStyleElement = () => {
+  const cssText = /* CSS */ `
+#viewer-extension-root {
+  position: fixed;
+  bottom: 0;
+  z-index: 9999999;
+}
+
+.indicator {
+  color: red;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding: 5px;
+  background: #eee;
+  border: 1px solid #aaa;
+  cursor: pointer;
+  z-index: 9999;
+}
+`
+
+  // Add css for the indicator
+  const style = createElement('style', { id: 'viewer-extension-style', innerHTML: cssText })
+  window.document.head.appendChild(style)
+}
+
 if (window.top !== window) {
   // Update the storage with the current viewer name
   const thunderboltScript = document.querySelector('script[src*="tb-main/dist/tb-main.js"]')
   new Storage().set('editor-indicator', thunderboltScript ? 'Thunderbolt' : 'Bolt')
 } else {
-  // Add css for the indicator
-  const style = createElement('style', { id: 'viewer-extension-style', textContext: cssText })
-  window.document.head.appendChild(style)
-
+  addStyleElement()
   // Add indicator element and the css of it to the document
   const rootElement = createElement('div', { id: 'viewer-extension-root' })
   window.document.body.appendChild(rootElement)
 }
 
 export const getRootContainer = async () => {
-  return document.querySelector('#viewer-extension-root')
-}
-
-const onClick = (viewerName: string) => {
-  const tbExperiments = {
-    'editor.wix.com': 'specs.UseTbInPreview',
-    'create.editorx.com': 'specs.UseTbInPreviewForResponsive',
-    'blocks.wix.com': 'specs.UseTbInPreviewForBlocks',
-  }
-
-  const experiment = tbExperiments[window.location.host]
-  const url = new URL(window.location.href)
-  const setPetriOvr = createSetPetriOvr(url)
-
-  setPetriOvr(viewerName !== 'Thunderbolt', experiment, true)
-  window.location.href = url.href
+  return window.top === window ? document.querySelector('#viewer-extension-root') : document.createElement('div')
 }
 
 const Indicator = () => {
+  const onClick = (viewerName: string) => {
+    const tbExperiments = {
+      'editor.wix.com': 'specs.UseTbInPreview',
+      'create.editorx.com': 'specs.UseTbInPreviewForResponsive',
+      'blocks.wix.com': 'specs.UseTbInPreviewForBlocks',
+    }
+
+    const experiment = tbExperiments[window.location.host]
+    const url = new URL(window.location.href)
+    const setPetriOvr = createSetPetriOvr(url)
+
+    setPetriOvr(viewerName !== 'Thunderbolt', experiment, true)
+    window.location.href = url.href
+  }
+
   const [viewerName] = useStorage('editor-indicator')
   return (
     <Box onClick={() => onClick(viewerName)} className="indicator">
